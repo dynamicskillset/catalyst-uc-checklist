cb2e14f3bbd9cd7b96e0877a4f0ec996e66973ab
ivan minutillo <minutillo.ivan@gmail.com>
2021-02-18 23:13:09 +0100

    improvement

---
+++ languages.js
@@ -0,0 +1,23 @@
+function english() {
+    return {
+      title: 'Universal credit checklist',
+      description: 'Additional information about the checklist, it may contains some links to other pages or not.',
+      phone: 'A mobile phone number',
+      email: 'An email address',
+      bank: 'A bank account',
+      partner: 'A partner account (if applicable)',
+      button: 'Success!'
+    }
+}
+
+function italian() {
+    return {
+      title: 'Checklist per il credito universale',
+      description: 'Additional information about the checklist, it may contains some links to other pages or not.',
+      phone: 'A mobile phone number',
+      email: 'An email address',
+      bank: 'A bank account',
+      partner: 'A partner account (if applicable)',
+      button: 'Success!'
+    }
+}
